f594b82e5230e85457b57ec9bb2e18b603726b0d
kamal-mobile-dev <kamal.verma@gamiotech.com>
2024-12-17 15:49:48 +0530

    push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Header';
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
---
+++ src/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+const Header = () => {
+  return <div className="header"></div>;
+};
+
+export default Header;
